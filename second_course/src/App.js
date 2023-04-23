7cdd1d16880e4b679f6a7f8b48551e10159e3a07
Ram85-git <126403725+Ram85-git@users.noreply.github.com>
2023-04-23 18:59:39 -0400

    saved it

---
+++ second_course/src/App.js
@@ -0,0 +1,59 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useState } from 'react';
+import './App.css';
+import Fields from './components/Fields';
+import Header from './components/Header';
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data , setData] = useState([]);
+
+const addData = () => {
+  setData ([ ...data, { name, email }]);
+  setName("");
+  setEmail("");
+}
+
+  return (
+    <div className="App">
+      <Header />
+      <div className='form'>
+        <Stack direction="row" spacing={2}>
+          <TextField value={name}
+           onChange={(event) => setName(event.target.value)} 
+          id="outlined-basic" label="name" variant="outlined" />
+          <TextField value={email} 
+          onChange={(event) => setEmail(event.target.value)}
+          id="outlined-basic" label="email" variant="outlined" />
+
+          <Button onClick={addData} variant="contained">
+            Add
+          </Button>
+        </Stack>
+      </div>
+      <div className='data'>
+        <div className='data_val'>
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {
+          data.map((element, index) =>{
+             return(
+              <Fields key ={index} name={element.name} email ={element.email} index = {index} />
+             )
+
+          })
+        }
+
+        
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
